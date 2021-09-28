ff5779e67913a53e7e83d7989c04bcc5e658a890
arigova <arigova@smartdatatech.io>
2021-09-28 14:11:58 +0300

    films2

---
+++ src/pages/404.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="not-found">
+    <h1>
+      Ничего не найдено
+    </h1>
+    <router-link
+        :to="{ name: 'films' }"
+    >
+      <p>Фильмы</p>
+    </router-link>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .not-found {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    min-height: 100vh;
+    h1 {
+      color: #ffffff;
+      font-size: 70px;
+      margin-bottom: 20px;
+    }
+    p {
+      text-decoration: none;
+      font-weight: 700;
+      color: #ffffff;
+      font-size: 25px;
+    }
+  }
+</style>
---
+++ src/pages/MainPage.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="main-page">
+    <h1>
+      Онлайн кинотеатр
+    </h1>
+    <button type="button" class="btn btn-outline-light" @click="$router.push({ name: 'films' })">Перейти к фильмам</button>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .main-page {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    min-height: 100vh;
+    h1 {
+      color: #ffffff;
+      font-size: 70px;
+      margin-bottom: 20px;
+    }
+  }
+</style>
